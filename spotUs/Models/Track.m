892a06b39758306798e55a9a8b9d09598a0d0ce2
Megan Ung <meganu@fb.com>
2018-07-16 13:23:52 -0700

    Track obj and edited city obj

---
+++ spotUs/Models/Track.m
@@ -0,0 +1,18 @@
+//
+//  Track.m
+//  spotUs
+//
+//  Created by Megan Ung on 7/16/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import "Track.h"
+
+@implementation Track
+@dynamic spotifyID;
+
++ (nonnull NSString *)parseClassName {
+    return @"Track";
+}
+
+@end
