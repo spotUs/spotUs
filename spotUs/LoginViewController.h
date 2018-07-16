c149a3b7e4f2500a1f37e16ee72a0aca912fde59
Megan Ung <meganu@fb.com>
2018-07-16 13:35:07 -0700

    parse users login VC

---
+++ spotUs/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  spotUs
+//
+//  Created by Megan Ung on 7/16/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+
+@interface LoginViewController : UIViewController
+
+@end
