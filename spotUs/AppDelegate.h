14ce71181f87fcf90d0728c2866deba787af2256
Martin Winton <mwinton@mit.edu>
2018-07-16 09:44:34 -0700

    added frameworks and set up login fon for spotify

--- spotUs/AppDelegate.h
+++ spotUs/AppDelegate.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SafariServices/SafariServices.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, SPTAudioStreamingDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
