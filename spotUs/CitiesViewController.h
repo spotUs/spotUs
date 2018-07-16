75ac6bcec49ac151b05ac4b52eb8da4e6724f202
Megan Ung <meganu@fb.com>
2018-07-16 15:25:17 -0700

    fixing merge conflict

---
+++ spotUs/CitiesViewController.h
@@ -0,0 +1,13 @@
+//
+//  CitiesViewController.h
+//  spotUs
+//
+//  Created by Megan Ung on 7/16/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CitiesViewController : UIViewController
+
+@end
