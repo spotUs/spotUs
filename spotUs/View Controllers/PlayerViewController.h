20924b3cc20b81b19f2a0c703d1c53177f336f85
Martin Winton <mwinton@mit.edu>
2018-07-16 10:29:51 -0700

    created new view controller for player

---
+++ spotUs/View Controllers/PlayerViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayerViewController.h
+//  spotUs
+//
+//  Created by Martin Winton on 7/16/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayerViewController : UIViewController
+
+@end
---
+++ spotUs/View Controllers/PlayerViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlayerViewController.m
+//  spotUs
+//
+//  Created by Martin Winton on 7/16/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import "PlayerViewController.h"
+
+@interface PlayerViewController ()
+
+@end
+
+@implementation PlayerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
