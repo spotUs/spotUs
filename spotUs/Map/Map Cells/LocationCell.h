2da2a70ffcdf5840dacad703648480ffb0adfa0b
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-07-17 15:19:28 -0700

    mapKit

---
+++ spotUs/Map/Map Cells/LocationCell.h
@@ -0,0 +1,15 @@
+//
+//  LocationCell.h
+//  PhotoMap
+//
+//  Created by emersonmalca on 7/8/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LocationCell : UITableViewCell
+
+- (void)updateWithLocation:(NSDictionary *)location;
+
+@end
---
+++ spotUs/Map/Map View Controllers/PhotoMapViewController.m
@@ -0,0 +1,89 @@
+//
+//  LocationsViewController.m
+//  PhotoMap
+//
+//  Created by Lizbeth Alejandra Gonzalez on 7/17/18.
+//  Copyright © 2018 Codepath. All rights reserved.
+//
+
+#import "LocationsViewController.h"
+#import "LocationCell.h"
+
+static NSString * const clientID = @"QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL";
+static NSString * const clientSecret = @"W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU";
+
+@interface LocationsViewController () <UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *results;
+
+@end
+
+@implementation LocationsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    self.searchBar.delegate = self;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.results.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    LocationCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LocationCell" forIndexPath:indexPath];
+    [cell updateWithLocation:self.results[indexPath.row]];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // This is the selected venue
+    NSDictionary *venue = self.results[indexPath.row];
+    NSNumber *lat = [venue valueForKeyPath:@"location.lat"];
+    NSNumber *lng = [venue valueForKeyPath:@"location.lng"];
+    NSLog(@"%@, %@", lat, lng);
+    
+}
+
+- (BOOL)searchBar:(UISearchBar *)searchBar shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
+    NSString *newText = [searchBar.text stringByReplacingCharactersInRange:range withString:text];
+    [self fetchLocations:newText nearCity:@"San Francisco"];
+    return true;
+}
+
+- (void) tapSearchBarSearchButton:(UISearchBar *)searchBar {
+    [self fetchLocations:searchBar.text nearCity:@"San Francisco"];
+}
+
+- (void)fetchLocations:(NSString *)query nearCity: (NSString *)city {
+    NSString *baseURLString = @"https://api.foursquare.com/v2/venues/search?";
+    NSString *queryString = [NSString stringWithFormat:@"client_id=%@&client_secret=%@&v=20141020&near=%@,CA&query=%@", clientID, clientSecret, city, query];
+    queryString = [queryString stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
+    
+    NSURL *url = [NSURL URLWithString:[baseURLString stringByAppendingString:queryString]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (data) {
+            NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+            NSLog(@"response: %@", responseDictionary);
+            self.results = [responseDictionary valueForKeyPath:@"response.venues"];
+            [self.tableView reloadData];
+        }
+    }];
+    [task resume];
+}
+
+@end
+
