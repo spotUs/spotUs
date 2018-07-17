7f4e1758760f261a49f81cd5d1ec2dfa67eca16d
Megan Ung <meganu@fb.com>
2018-07-17 10:21:44 -0700

    search cities view controller

--- spotUs/CitiesViewController.m
+++ spotUs/CitiesViewController.m
@@ -7,13 +7,14 @@
 //
 
 #import "CitiesViewController.h"
+#import "City.h"
 
 @interface CitiesViewController () <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate>
 //TODO CONNECT THESE
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
-@property (strong, nonatomic) NSMutableArray *cities;
+@property (strong, nonatomic) NSArray *cities;
 @property (strong, nonatomic) NSArray *filteredCities;
 @end
 
@@ -21,7 +22,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    [self fetchCities];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     self.searchBar.delegate = self;
@@ -32,22 +33,72 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)fetchCities{
+    PFQuery *query = [PFQuery queryWithClassName:@"City"];
+    query.limit = 20;
+    [query includeKey:@"lng"];
+    [query includeKey:@"lat"];
+    [query includeKey:@"name"];
+    [query includeKey:@"tracks"];
+    [query orderByAscending:@"name"];
+    
+    [query findObjectsInBackgroundWithBlock:^(NSArray *cities, NSError *error) {
+        if (!error) {
+            self.cities = cities;
+            if (self.filteredCities == nil){
+                self.filteredCities = cities;
+            }
+            NSLog(@"%@",self.filteredCities);
+            [self.tableView reloadData];
+        } else {
+            NSLog(@"%@", error.localizedDescription);
+        }
+    }];
 }
-*/
 
 - (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
-    return nil;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CityCell"];
+    City *city = self.filteredCities[indexPath.row];
+    cell.textLabel.text = city.name;
+    return cell;
 }
 
 - (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.filteredCities.count;
 }
+
+- (void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    if (searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *evaluatedObject, NSDictionary *bindings){
+            return [[evaluatedObject[@"name"] lowercaseString] containsString:[searchText lowercaseString]];
+        }];
+        self.filteredCities
+        = [self.cities filteredArrayUsingPredicate:predicate];
+        NSLog(@"%@",self.filteredCities);
+    } else {
+        self.filteredCities = self.cities;
+    }
+    [self.tableView reloadData];
+}
+
+- (void) searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    //close the keyboard that is toggled when searching
+    [searchBar resignFirstResponder];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 
 @end
