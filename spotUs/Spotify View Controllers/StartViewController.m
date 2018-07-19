ebbb07406d5c7a649302f817abca9432eaf9a761
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-07-19 11:25:25 -0700

    added new gif storyboard

--- spotUs/Spotify View Controllers/StartViewController.m
+++ spotUs/Spotify View Controllers/StartViewController.m
@@ -17,7 +17,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+   
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,6 +37,8 @@
     self.delegate = loginView;
     
 }
+
+
 
 
 @end
