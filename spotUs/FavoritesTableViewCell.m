aac09de50398e3dba8eb5413af61e2da14c7d674
Lizbeth Alejandra Gonzalez <lizgonzalez@fb.com>
2018-07-20 15:43:44 -0700

    added storuboard and cells forfavorites

---
+++ spotUs/FavoritesTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  FavoritesTableViewCell.m
+//  spotUs
+//
+//  Created by Lizbeth Alejandra Gonzalez on 7/20/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import "FavoritesTableViewCell.h"
+
+@implementation FavoritesTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
