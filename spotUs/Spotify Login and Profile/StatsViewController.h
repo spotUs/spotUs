07f905eee5e253ca1ef064fe5f2b58a34256639e
Martin Winton <mwinton@mit.edu>
2018-07-30 16:51:44 -0700

    added info view to storyboard

---
+++ spotUs/Spotify Login and Profile/StatsViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatsViewController.h
+//  spotUs
+//
+//  Created by Martin Winton on 7/30/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatsViewController : UIViewController
+
+@end
