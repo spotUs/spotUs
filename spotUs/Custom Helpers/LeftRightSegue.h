f764231cf09c610b223274543c8832e475464ec8
meganung <ung.meg@gmail.com>
2018-07-30 12:41:06 -0700

    added custom Left to Right segue when press favorites

---
+++ spotUs/Custom Helpers/LeftRightSegue.h
@@ -0,0 +1,14 @@
+//
+//  LeftRightSegue.h
+//  spotUs
+//
+//  Created by Megan Ung on 7/30/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+
+@interface LeftRightSegue : UIStoryboardSegue
+
+@end
