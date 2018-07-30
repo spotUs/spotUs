2292ef176a5b165210305e3279ee6de1a7aa4352
Martin Winton <mwinton@mit.edu>
2018-07-30 13:21:28 -0700

    changed favorites format to match playlist format

---
+++ spotUs/PlayLists/Playlist Cells/PlayListTableHeader.m
@@ -0,0 +1,24 @@
+//
+//  PlayListTableHeader.m
+//  spotUs
+//
+//  Created by Martin Winton on 7/30/18.
+//  Copyright © 2018 Lizbeth Alejandra Gonzalez. All rights reserved.
+//
+
+#import "PlayListTableHeader.h"
+
+@implementation PlayListTableHeader
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
