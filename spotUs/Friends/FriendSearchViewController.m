213c5d9df9f42c26b7fb5d0be0d55dd6b24413c3
meganung <ung.meg@gmail.com>
2018-07-31 09:23:50 -0700

    added refresh for friends VC

--- spotUs/Friends/FriendSearchViewController.m
+++ spotUs/Friends/FriendSearchViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
 
+@property (strong, nonatomic) UIRefreshControl *refreshControl;
 @property (strong, nonatomic) NSArray <PFUser *> *users;
 @property (strong, nonatomic) NSArray <PFUser *> *filteredFriends;
 @property (strong, nonatomic) NSArray <PFUser *> *filteredNotFriends;
@@ -31,6 +32,10 @@
     
     self.searchBar.delegate = self;
     
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(didPullToRefresh:) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+    
     self.usersFriends = [NSArray array];
     self.usersNotFriends = [NSArray array];
     self.filteredFriends = self.usersFriends;
@@ -45,6 +50,10 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void) didPullToRefresh:(UIRefreshControl *)refreshControl{
+    [self updateUsers];
+}
+
 - (void) updateUsers {
     // get users that are not current user and friends
     PFQuery *friendQuery = [PFUser query];
@@ -52,10 +61,14 @@
     [friendQuery whereKey:@"username" containedIn:[PFUser currentUser][@"friends"]];
     [friendQuery orderByAscending:@"username"];
     [friendQuery findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-        self.usersFriends = objects;
-        self.filteredFriends = objects;
-        NSLog(@"filteredfriends %@",self.filteredFriends);
-        [self.tableView reloadData];
+        if (error){
+            NSLog(@"error fetching friends: %@",error.localizedDescription);
+        } else {
+            self.usersFriends = objects;
+            self.filteredFriends = objects;
+            NSLog(@"filteredfriends %@",self.filteredFriends);
+            [self.tableView reloadData];
+        }
     }];
     // get non friends
     PFQuery *notFriendQuery = [PFUser query];
@@ -63,9 +76,14 @@
     [notFriendQuery whereKey:@"username" notContainedIn:[PFUser currentUser][@"friends"]];
     [notFriendQuery orderByAscending:@"username"];
     [notFriendQuery findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
-        self.usersNotFriends = objects;
-        self.filteredNotFriends = objects;
-        [self.tableView reloadData];
+        if (error) {
+            NSLog(@"error fetching non friends: %@", error.localizedDescription);
+        } else {
+            self.usersNotFriends = objects;
+            self.filteredNotFriends = objects;
+            [self.tableView reloadData];
+        }
+        [self.refreshControl endRefreshing];
     }];
 }
 
@@ -110,6 +128,10 @@
 
 - (void) searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     [searchBar resignFirstResponder];
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:true];
 }
 
 /*
